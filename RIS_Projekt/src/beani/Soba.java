1108c1c7c156b57f3b160870af550fbb4f33ca75
Jure Žerak <jure.zerak@gmail.com>
2015-04-24 14:47:56 +0200

    Dodal rezervacija miz in beane
    Dodal xhtml fajle za rezervacijo miz (samo "template" ker nimam baze) in
    java beane

---
+++ RIS_Projekt/src/beani/Soba.java
@@ -0,0 +1,28 @@
+package beani;
+
+public class Soba {
+	
+	private int stSobe;
+	private int kapaciteta;
+	private String tip;
+	
+	public int getStSobe() {
+		return stSobe;
+	}
+	public void setStSobe(int stSobe) {
+		this.stSobe = stSobe;
+	}
+	public int getKapaciteta() {
+		return kapaciteta;
+	}
+	public void setKapaciteta(int kapaciteta) {
+		this.kapaciteta = kapaciteta;
+	}
+	public String getTip() {
+		return tip;
+	}
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+	
+}
